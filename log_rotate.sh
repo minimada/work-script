05b5e671ea86dddb00712a5387eb04c717063277
Brian Ma <chma0@nuvoton.com>
2021-03-16 16:48:04 +0800

    add filter function for log rotate per each project
    Signed-off-by: Brian Ma <chma0@nuvoton.com>

--- log_rotate.sh
+++ log_rotate.sh
@@ -1,58 +1,67 @@
 #!/bin/bash
 #
-#
+# test command: -l /home2/cs20/test_logs -i /home2/cs20/images_backup -f buv-runbmc:olympus-nuvoton
 # 
 
 Usage(){
-    echo `basename $0` "[-ijlm data]"
+    echo `basename $0` "[-ijlmf data]"
     echo "      -i : set data as image path"
     echo "      -j : set data as image max"
     echo "      -l : set data as log path"
     echo "      -m : set data as log max"
+    echo "      -f : set data as backup project folder"
     exit 1
 }
 
 # Definitions
 LOG_MAX=30
 IMAGE_MAX=30
-LOG_PATH=/home2/cs20/test_logs
-IMAGE_PATH=/home2/cs20/images_backup
+# use /home2 for debug environment
+LOG_PATH=/home/cs20/test_logs
+IMAGE_PATH=/home/cs20/images_backup
 
 # functions
 # need direct set globle var or use local 
 get_image_count(){
-	find $IMAGE_PATH -maxdepth 1 -type f | wc -l
+	find $1 -maxdepth 1 -type f | wc -l
 }
 
 get_log_count(){
-	find $LOG_PATH -maxdepth 1 -mindepth 1 -type d | wc -l
+	find $1 -maxdepth 1 -mindepth 1 -type d | wc -l
 }
 
 # $1 : log or image path
 # $2 : max count
 # $3 : run log or image
+# $4 : filter for handler each project
 rotate(){
-	if [ ! -d "$1" ];then
-		echo "cannot find path: $1"
+	if [ ! -d "${1}/${4}" ];then
+		echo "cannot find path: ${1}/${4}"
 		return
 	else
-		local path="$1"
+		local path="${1}/${4}"
 		local max="$2"
 	fi
 
 	if [ "$verbose" == "y" ];then
-		echo "Type: $3"
-		echo "Path: $path"
-		echo "Max : $max"
+		echo " "
+		echo "Type  : $3"
+		echo "Path  : $path"
+		echo "Max   : $max"
+		echo "Filter: $4"
 	fi	
 
 	if [ "$3" == "LOG" ];then
-		local count=$(get_log_count)
+		local count=$(get_log_count ${path})
 	elif [ "$3" == "IMAGE" ];then
-		local count=$(get_image_count)
+		local count=$(get_image_count ${path})
 	else
 		echo "invalid type"
 		return
+	fi
+
+	if [ "$verbose" == "y" ];then
+		echo "Count : $count"
 	fi
 
 	if [ "$count" -gt "$max" ];then
@@ -71,7 +80,7 @@ rotate(){
 }
 
 parse_arg(){
-	while getopts ":vdni:j:l:m:" argv;do
+	while getopts ":vdni:j:l:m:f:" argv;do
 		case "$argv" in
 			i)
 				IMAGE_PATH=${OPTARG}
@@ -94,6 +103,11 @@ parse_arg(){
 			d)
 				verbose="y"
 				not_remove="n"
+				debug="y"
+				;;
+			f)
+				# like Buv:Olympus
+				IMAGE_FILTERS=${OPTARG}
 				;;
 			*)
 				Usage
@@ -101,13 +115,20 @@ parse_arg(){
 		esac
 	done
 	shift $((OPTIND-1))
+	IMAGE_FILTERS=`echo $IMAGE_FILTERS | tr ":" " "`
+	if [ "$debug" == "y" ];then
+		echo "FILTERS: $IMAGE_FILTERS"
+	fi
 }
 
 # Main
 parse_arg $@
 
-rotate $LOG_PATH $LOG_MAX "LOG"
+for filter in $IMAGE_FILTERS
+do
+	rotate $LOG_PATH $LOG_MAX "LOG" $filter
 
-rotate $IMAGE_PATH $IMAGE_MAX "IMAGE"
+	rotate $IMAGE_PATH $IMAGE_MAX "IMAGE" $filter
+done
 
 exit 0
